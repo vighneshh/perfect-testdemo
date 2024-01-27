09a3256405d0fc47c39669969a5144a3e3f5b6f9
Vighnesh Gawad <37585674+vighneshh@users.noreply.github.com>
2024-01-27 18:43:25 +0530

    perfect files

---
+++ hello-prefect.py
@@ -0,0 +1,24 @@
+from prefect import flow, task
+from prefect.blocks.system import String
+
+string_block = String.load("vtest")
+
+@task
+def create_msg():
+	msg = "Hello Prefect from task"
+	return msg
+
+@flow
+def hello_subflow():
+	submsg = "Subflow"
+	return submsg
+
+@flow
+def hello_world():
+	submsg = hello_subflow()
+	msg = create_msg()
+	whole_msg = msg + " , " + submsg + " and " + string_block.value
+	print(whole_msg)
+
+if __name__ == "__main__":
+	hello_world()
